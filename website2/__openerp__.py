cd13de1120ed0b7c0399c9565c1fbb8434fafd6a
Vipul Bhatt <vipul.bh@vperfectcs.com>
2017-04-21 15:36:26 +0530

    [ADD] website2

---
+++ website2/__openerp__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Website 2',
+    'category': 'Website',
+    'description': "Website 2 - Depends on Website 1",
+    'author': 'V Perfect',
+    'depends': ['website1'],
+    'data': [
+        'views/website_templates.xml',
+    ],
+    'installable': True,
+}
